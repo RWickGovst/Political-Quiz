fe6b897ed2fe5a205eedb88f53300b81253218bf
Triumph <archie200@icloud.com>
2019-07-16 14:07:30 -0500

    schema changes

--- models/schema.sql
+++ models/schema.sql
@@ -1,15 +1,53 @@
 DROP DATABASE IF EXISTS political_quiz_db;
 CREATE DATABASE political_quiz_db;
 
+DROP DATABASE IF EXISTS political_quiz_db;
+CREATE DATABASE political_quiz_db;
+
 USE political_quiz_db;
 
-CREATE TABLE political_quiz (
+USE political_quiz_db;
+
+CREATE TABLE user_results (
   
   id INTEGER(11) AUTO_INCREMENT NOT NULL,
  
  complete BOOLEAN DEFAULT false,
 
  result VARCHAR(30) NOT NULL,
-
+q1 VARCHAR(10),
+q2 VARCHAR(10),
+q3 VARCHAR(10),
+q4 VARCHAR(10),
+q5 VARCHAR(10),
+q6 VARCHAR(10),
+q7 VARCHAR(10),
+q8 VARCHAR(10),
+q9 VARCHAR(10),
+q10 VARCHAR(10),
+q11 VARCHAR(10),
+q12 VARCHAR(10),
+q13 VARCHAR(10),
+q14 VARCHAR(10),
+q15 VARCHAR(10),
+q16 VARCHAR(10),
+q17 VARCHAR(10),
+q18 VARCHAR(10),
+q19 VARCHAR(10),
+q20 VARCHAR(10),
 PRIMARY KEY (id)
 );
+
+SELECT * FROM user_results;
+CREATE TABLE questions (
+id INTEGER(11) AUTO_INCREMENT NOT NULL,
+question VARCHAR(200),
+ans1 VARCHAR(10),
+ans2 VARCHAR(10),
+ans3 VARCHAR(10),
+ans4 VARCHAR(10),
+ans5 VARCHAR(10),
+PRIMARY KEY (id)
+);
+
+SELECT * FROM questions
