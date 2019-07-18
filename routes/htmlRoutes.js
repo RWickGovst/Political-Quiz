f21abfeea4e18e5bd937a8e62d5a1471f94c2ea8
Spencer Rose <spencerkrose@gmail.com>
2019-07-18 12:57:33 -0500

    render routing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/styles/index.html"));
+    res.render("index");
   });
 
   // cms route loads cms.html
