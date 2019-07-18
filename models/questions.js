234aee4a3832e0123606613ac2b69e127c48f545
Triumph <archie200@icloud.com>
2019-07-18 13:36:10 -0500

    seeds

---
+++ models/questions.js
@@ -0,0 +1,13 @@
+module.exports = function (sequelize, DataTypes) {
+    var Questions = sequelize.define("Questions", {
+        question: DataTypes.STRING,
+        category: DataTypes.STRING,
+        rep: DataTypes.INTEGER,
+        const: DataTypes.INTEGER,
+        green: DataTypes.INTEGER,
+        lib: DataTypes.INTEGER,
+        dem: DataTypes.INTEGER,
+
+    });
+    return Questions;
+};
