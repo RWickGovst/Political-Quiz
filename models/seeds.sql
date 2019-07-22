f82a9d9a866339f21a24caf454095b0d23bad0c2
Triumph <archie200@icloud.com>
2019-07-22 10:04:15 -0500

    uncommented seeds

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,23 +1,23 @@
--- INSERT INTO questions
---     ( question, category)
--- VALUES
--- ("The unborn child has a fundamental individual right to life which cannot be infringed.", "Social Issues"),
--- ("Same sex marriage should not be accepted by society.","Social Issues"),
--- ("Abortion is a woman’s unrestricted right.","Social Issues"),
--- ("Education is best provided by the free market, achieving greater quality and efficiency with more diversity of choice.","Social Issues"),
--- ("Institutional racism is the main reason why many minorities can't get ahead these days.","Social Issues"),
--- ("The best way to ensure peace is through military strength.","International Issues"),
--- ("No funds should be appropriated for any kind of foreign aid program.","International Issues"),
--- ("Our government should sign the International Criminal Court agreement and respect the authority of that institution.","International Issues"),
--- ("Free trade engenders positive international relationships.","International Issues"),
--- ("Good diplomacy is the best way to ensure peace.","International Issues"),
--- ("A free market healthcare system is the best solution to providing the greatest possible care to the most Americans.","Domestic Issues"),
--- ("The government must not spend more than it takes in.","Domestic Issues"),
--- ("The Internet is a commons, developed with public funds, and must insure freedom and equality of access.","Domestic Issues"),
--- ("Oppose all government intervention and regulation on wages, prices, profits, production and interest rates.","Domestic Issues"),
--- ("Health care is a right, not a privilege, and our health care system should put people before profits.","Domestic Issues"),
--- ("Creating a low tax environment for job creators is the best way to stimulate growth.","Economic Issues"),
--- ("Congress shall not levy taxes on personal incomes, estates, and/or gifts.","Economic Issues"),
--- ("Government regulation of business is necessary to protect the public interest.","Economic Issues"),
--- ("The only role of government in the economy is to protect property rights, adjudicate disputes, and provide a legal framework in which voluntary trade is protected.","Economic Issues"),
--- ("The wealthiest Americans and largest corporations must pay their fair share of taxes.","Economic Issues")
+INSERT INTO questions
+    ( question, category)
+VALUES
+("The unborn child has a fundamental individual right to life which cannot be infringed.", "Social Issues"),
+("Same sex marriage should not be accepted by society.","Social Issues"),
+("Abortion is a woman’s unrestricted right.","Social Issues"),
+("Education is best provided by the free market, achieving greater quality and efficiency with more diversity of choice.","Social Issues"),
+("Institutional racism is the main reason why many minorities can't get ahead these days.","Social Issues"),
+("The best way to ensure peace is through military strength.","International Issues"),
+("No funds should be appropriated for any kind of foreign aid program.","International Issues"),
+("Our government should sign the International Criminal Court agreement and respect the authority of that institution.","International Issues"),
+("Free trade engenders positive international relationships.","International Issues"),
+("Good diplomacy is the best way to ensure peace.","International Issues"),
+("A free market healthcare system is the best solution to providing the greatest possible care to the most Americans.","Domestic Issues"),
+("The government must not spend more than it takes in.","Domestic Issues"),
+("The Internet is a commons, developed with public funds, and must insure freedom and equality of access.","Domestic Issues"),
+("Oppose all government intervention and regulation on wages, prices, profits, production and interest rates.","Domestic Issues"),
+("Health care is a right, not a privilege, and our health care system should put people before profits.","Domestic Issues"),
+("Creating a low tax environment for job creators is the best way to stimulate growth.","Economic Issues"),
+("Congress shall not levy taxes on personal incomes, estates, and/or gifts.","Economic Issues"),
+("Government regulation of business is necessary to protect the public interest.","Economic Issues"),
+("The only role of government in the economy is to protect property rights, adjudicate disputes, and provide a legal framework in which voluntary trade is protected.","Economic Issues"),
+("The wealthiest Americans and largest corporations must pay their fair share of taxes.","Economic Issues")
